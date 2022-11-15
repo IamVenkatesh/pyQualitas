3bee11b3c00333c131ff2a62589d4be60d7d4eef
Venkatesh Venkataramani <venkatesh.venkataramani@gmail.com>
2022-11-15 13:14:43 +0530

    Merging Release branch into Main (#14)
    * Modifying setup.py file to check if the package can be used
    
    * Fixed the package data issue and Readme file
    
    * Adding package directory to setup file
    
    * Fixed the import statements
    
    * Fixed import statements of unit tests
    
    * Adding PYTHONPATH in Github Action workflow
    
    * Adding src directory to PYTHONPATH
    
    * Fixed the operating system classifier in setup file
    
    * Enhancement to check duplicates function
    
    * Modified & fixed the issue in check duplicates method
    
    * Fixed the package data path to relative path
    
    * Changing the version and marking it as a stable release

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 setup(
     name="pyQualitas",
-    version="1.0.3",
+    version="1.0.5",
     description="A project to ensure the data quality using python",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
@@ -12,7 +12,7 @@ setup(
     url="https://github.com/IamVenkatesh/pyQuality/wiki",
     packages=['pyqualitas', 'pyqualitas/checks', 'pyqualitas/checksuite', 'pyqualitas/utils'],
     package_dir={"":"src"},
-    package_data={'pyqualitas/utils': ['pyqualitas/utils/template/TestResult.html']},
+    package_data={'pyqualitas/utils': ['template/TestResult.html']},
     install_requires=[
         "pyspark >= 3.3.0",
         "Jinja2 >= 3.1.2",
@@ -22,7 +22,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: Unix",
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Topic :: Software Development :: Quality Assurance",
         "Topic :: Software Development :: Testing"
     ],
